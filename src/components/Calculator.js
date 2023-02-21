b850f861546463fc5d5d0524cf63339e1965cf25
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-02-21 15:31:03 +0100

    Create react component in calculators.js file

---
+++ src/components/Calculator.js
@@ -0,0 +1,26 @@
+const Calculator = () => (
+  <div className="container">
+    <div className="result">0</div>
+    <div className="key">AC</div>
+    <div className="key">+/-</div>
+    <div className="key">%</div>
+    <div className="operator">AC</div>
+    <div className="key">7</div>
+    <div className="key">8</div>
+    <div className="key">9</div>
+    <div className="operator">*</div>
+    <div className="key">4</div>
+    <div className="key">5</div>
+    <div className="key">6</div>
+    <div className="operator">-</div>
+    <div className="key">1</div>
+    <div className="key">2</div>
+    <div className="key">3</div>
+    <div className="operator">+</div>
+    <div className="key">0</div>
+    <div className="key">.</div>
+    <div className="operator">=</div>
+  </div>
+);
+
+export default Calculator;
