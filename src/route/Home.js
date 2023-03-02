b9cbfb66224d91fb26d0cb87e552538e71bd9835
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-02 14:00:00 +0100

    npm i react-router-dom

--- src/route/Home.js
+++ src/route/Home.js
@@ -6,6 +6,8 @@ const Home = () => (
 
     <p>
       Math Magic is a handy tool for quick and easy solutions to basic math problems.
+      <br />
+
       Ideal for students, professionals, and anyone who needs to perform calculations regularly.
 
       It performs basic math operations like addition, multiplication, division and subtraction.
